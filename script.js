b98ae2438cd26a091aeeefdfc7e587086a8f9955
mikels0n <myskamichal@gmail.com>
2023-11-02 10:08:02 +0100

    Reset and Grid Size buttons added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const sketchpad = document.querySelector('.sketchpad');
+const erease = document.querySelector('#erease');
+const gridSize = document.querySelector('#grid-size');
 
 function createGrid(divsPerLine = 16) {
     sketchpad.innerHTML = "";
-    let size = 420/16 + 'px'
+    let size = 420 / divsPerLine + 'px'
     for (let i = divsPerLine * divsPerLine; i !== 0; i--) {
         const square = document.createElement('div');
         square.classList.add('square');
@@ -15,5 +17,20 @@ function createGrid(divsPerLine = 16) {
         
     }
 }
+
+erease.addEventListener('click', () => {
+    createGrid();
+});
+
+gridSize.addEventListener('click', () => {
+    let numberOfDivs = prompt("How many squares per side? (Maximum: 100)");
+    while (numberOfDivs > 100 || numberOfDivs < 1) {
+        numberOfDivs = prompt('Wrong number! Choose number between 1 and 100.');
+        if (numberOfDivs <= 100 && numberOfDivs > 0) {
+            break;
+        }
+    }
+    createGrid(numberOfDivs);
+})
 
 window.addEventListener('load', createGrid());
